de97a910659b34a822e8fa9dcf9a5db4867c18ba
Maxim Efimov <maxefi.ndo@gmail.com>
2016-11-08 17:12:09 +0300

    2.3.4. Redux initialisation on server side

--- src/server.js
+++ src/server.js
@@ -3,30 +3,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
 import routes from './routes';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
 
 const app = express();
 
-app.use(
-    (req, res) => {
-        match(
-            { routes, location: req.url }, (error, redirectLocation, renderProps) => {
-                if (redirectLocation) {
-                    return res.redirect(301, redirectLocation.pathname + redirectLocation.search);
-                }
-
-                if (error) {
-                    return res.status(500).send(error.message);
-                }
-
-                if (!renderProps) {
-                    return res.status(404).send('Not found');
-                }
-
-                const componentHTML = ReactDOM.renderToString(<RouterContext {...renderProps}/>)
-
-                return res.end(renderHTML(componentHTML));
+app.use((req, res) => {
+        match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+            if (redirectLocation) {
+                return res.redirect(301, redirectLocation.pathname + redirectLocation.search);
             }
-        );
+
+            if (error) {
+                return res.status(500).send(error.message);
+            }
+
+            if (!renderProps) {
+                return res.status(404).send('Not found');
+            }
+
+            const store = configureStore();
+
+            const componentHTML = ReactDOM.renderToString(
+                <Provider store={store}>
+                    <RouterContext {...renderProps}/>
+                </Provider>
+            );
+
+            return res.end(renderHTML(componentHTML));
+        });
     }
 );
 
