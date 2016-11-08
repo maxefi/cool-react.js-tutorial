f42aa205ade4bc80a36d00537cfea50a4b4bb455
Maxim Efimov <maxefi.ndo@gmail.com>
2016-11-08 17:16:34 +0300

    2.3.5. Redux initialisation on client side

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,7 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import counterReducer from './reducers/counterReducer';
 
-export default function(initialState = {}) {
+export default function (initialState = {}) {
     const rootReducer = combineReducers({
         counter: counterReducer
     });
--- src/redux/reducers/counterReducer.js
+++ src/redux/reducers/counterReducer.js
@@ -2,7 +2,7 @@ import { INCREMENT_COUNTER } from 'redux/actions/counterActions';
 
 const initialState = { value: 0 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
     switch (action.type) {
         case INCREMENT_COUNTER:
             return { value: state.value + 1 };
