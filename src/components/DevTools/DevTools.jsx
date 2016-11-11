12b0321590e1484569a6ee8d7baf8b04ca3ced89
Maxim Efimov <maxefi.ndo@gmail.com>
2016-11-11 12:22:42 +0300

    implemention some other DevTool for Redux

--- src/components/DevTools/DevTools.jsx
+++ src/components/DevTools/DevTools.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import Inspector from 'redux-devtools-inspector';
+import DiffMonitor from 'redux-devtools-diff-monitor';
+import LogMonitor from 'redux-devtools-log-monitor';
+import ChartMonitor from 'redux-devtools-chart-monitor';
 
 export default createDevTools(
-    <DockMonitor toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-q'>
-        <LogMonitor/>
+    <DockMonitor toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-q' changeMonitorKey='ctrl-m'>
+        <Inspector theme='harmonic'/>
+        <LogMonitor theme='apathy'/>
+        <DiffMonitor theme='apathy'/>
+        <ChartMonitor theme='apathy'/>
     </DockMonitor>
 );
